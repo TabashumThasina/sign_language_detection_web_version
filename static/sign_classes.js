9a26aaee10b42a930bbf9e161dfdeb9cb59fccd3
Thasina Tabashum <43305644+TabashumThasina@users.noreply.github.com>
2020-05-01 23:00:19 -0500

    Add files via upload

---
+++ static/sign_classes.js
@@ -0,0 +1,10 @@
+const SIGN_CLASSES = {
+    0: 'Aj (Today)',
+    1: 'Ami (Me)',
+    2: 'Bondhu (Friend)',
+    3: 'Onurodh (Request)',
+    4: 'Kashi ',
+    5: 'Khawa (Eat)',
+    6: 'Badha_dewa (Stop)',
+    7: 'Somoi (Time)'
+};
